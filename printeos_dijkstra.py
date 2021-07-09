56c2bb062cd8665ab543fb2b0bc5e5b58fda7a94
Ismael Contreras Morgado <correo@uc.clicontreras2@uc.cl>
2021-07-08 23:02:11 -0400

    ultimos pushs

---
+++ printeos_dijkstra.py
@@ -0,0 +1,16 @@
+
+print("")
+print("[RUTAS EVENTOS A CENTROS]")
+print("El tiempo que tomó calcular las 4319568 = 2727*66*24 rutas a tiempo mínimo fue de 31904.47626876831 segundos, equivalente a 531.7412711461385 minutos, equivalente a 8.862354519102308 horas. El tiempo promedio que tomó calcular cada ruta fue de 0.007386034035988856 segundos, equivalente a 0.00012310056726648092 minutos, equivalente a 2.0516761211080156e-06 horas")
+print("")
+
+
+print("")
+print("[RUTAS BASES A EVENTOS]")
+print("El tiempo que tomó calcular las 1701648 = 26*2727*24 rutas a tiempo mínimo fue de 11115.231620073318 segundos, equivalente a 185.2538603345553 minutos, equivalente a 3.0875643389092553 horas. El tiempo promedio que tomó calcular cada ruta fue de 0.0065320393054693554 segundos, equivalente a 0.0001088673217578226 minutos, equivalente a 1.8144553626303766e-06 horas")
+print("")
+
+print("")
+print("[RUTAS CENTROS A BASES]")
+print("El tiempo que tomó calcular las 41184 = 66*26*24 rutas a tiempo mínimo fue de 350.6549119949341 segundos, equivalente a 5.844248533248901 minutos, equivalente a 0.09740414222081502 horas. El tiempo promedio que tomó calcular cada ruta fue de 0.00851434809622509 segundos, equivalente a 0.00014190580160375148 minutos, equivalente a 2.365096693395858e-06 horas")
+print("")
